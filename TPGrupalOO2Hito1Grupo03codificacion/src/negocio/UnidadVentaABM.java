15bb31af6f81e35bb4676b856c1f574c71425770
anavazquez141 <anav4332816@gmail.com>
2026-09-03 16:06:25 -0300

    Mejoras en consultas

--- TPGrupalOO2Hito1Grupo03codificacion/src/negocio/UnidadVentaABM.java
+++ TPGrupalOO2Hito1Grupo03codificacion/src/negocio/UnidadVentaABM.java
@@ -50,11 +50,11 @@ public class UnidadVentaABM {
         return dao.traerFoodTracksConElectricidad(requiereElectricidad);
     }
 
-    public List<FoodTrack> traerFoodTracksConSuperficieMinima(double minSuperficie) {
+    public Set<FoodTrack> traerFoodTracksConSuperficieMinima(double minSuperficie) {
         return dao.traerFoodTracksPorSuperficieMinima(minSuperficie);
     }
 
-    public List<UnidadVenta> traerUnidadesVentaConMinimoPedidos(long minPedidos) {
+    public Set<UnidadVenta> traerUnidadesVentaConMinimoPedidos(long minPedidos) {
         return dao.traerUnidadesVentaConMinimoPedidos(minPedidos);
     }
 
